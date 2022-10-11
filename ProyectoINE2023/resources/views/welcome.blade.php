b8087a5b2233352bbb1d2bb4ba7b02620e2c12ac
Antonio Roldán Andrade <48240649+Mry3ll0w@users.noreply.github.com>
2022-10-11 16:40:28 +0200

    header listo :poop:

--- ProyectoINE2023/resources/views/welcome.blade.php
+++ ProyectoINE2023/resources/views/welcome.blade.php
@@ -12,12 +12,86 @@
 
     <title>Proyecto de INE</title>
   </head>
-
+  <header>
+    <style>
+      a {
+	        font-size: 14px;
+	        font-weight: 700
+	    }
+    
+	    .superNav {
+	        font-size: 13px;
+	    }
+    
+	    .form-control {
+	        outline: none !important;
+	        box-shadow: none !important;
+	    }
+    
+	    @media screen and (max-width:540px) {
+	      .centerOnMobile {
+	          text-align: center
+	      }
+	    }
+    </style>
+  
+    <nav class="navbar navbar-expand-lg bg-white sticky-top navbar-light p-3 shadow-sm">
+      <div class="container">
+        <a class="navbar-brand" href="#"><i class="fa-solid fa-shop me-2"></i> <strong>UCA SHOP</strong></a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+      
+        <div class="mx-auto my-3 d-lg-none d-sm-block d-xs-block">
+          <div class="input-group">
+            <span class="border-warning input-group-text bg-warning text-white"><i class="fa-solid fa-magnifying-glass"></i></span>
+            <input type="text" class="form-control border-warning" style="color:#7a7a7a">
+            <button class="btn btn-warning text-white">Buscar</button>
+          </div>
+        </div>
+        <div class=" collapse navbar-collapse" id="navbarNavDropdown">
+          <div class="ms-auto d-none d-lg-block">
+            <div class="input-group">
+              <span class="border-warning input-group-text bg-warning text-white"><i class="fa-solid fa-magnifying-glass"></i></span>
+              <input type="text" class="form-control border-warning" style="color:#7a7a7a">
+              <button class="btn btn-warning text-white">Buscar</button>
+            </div>
+          </div>
+          <ul class="navbar-nav ms-auto ">
+            <li class="nav-item">
+              <a class="nav-link mx-2 text-uppercase active" aria-current="page" href="#">Offers</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link mx-2 text-uppercase" href="#">Products</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link mx-2 text-uppercase" href="#">Catalog</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link mx-2 text-uppercase" href="#">Services</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link mx-2 text-uppercase" href="#">About</a>
+            </li>
+          </ul>
+          <ul class="navbar-nav ms-auto ">
+            <li class="nav-item">
+              <a class="nav-link mx-2 text-uppercase" href="#"><i class="fa-solid fa-cart-shopping me-1"></i> Cart</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link mx-2 text-uppercase" href="#"><i class="fa-solid fa-circle-user me-1"></i> Account</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  </header>
   <body>
   
-  <!-- FOOTER -->
   
-    <!-- Remove the container if you want to extend the Footer to full width. -->
+
+  <!-- FOOTER -->
+  </body>
   <div class="container my-5">
 
   <footer class="fixed-bottom text-center text-lg-start text-white" style="background-color: rgba(253, 159, 23, 100)">
@@ -107,5 +181,5 @@
 
 <!-- End of .container -->
   </div>  
-  </body>
+  
 </html>
