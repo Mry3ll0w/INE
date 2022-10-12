a882c7842bb527784a1158eca00e22d72166a493
Antonio Roldán Andrade <48240649+Mry3ll0w@users.noreply.github.com>
2022-10-12 11:31:10 +0200

    Con for hecho para evitar  repeticion de codigo

--- ProyectoINE2023/resources/views/welcome.blade.php
+++ ProyectoINE2023/resources/views/welcome.blade.php
@@ -110,63 +110,14 @@
         <div class='container px-4 px-lg-5 mt-5'>
           <div class='row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 '>
             
-            <div class='col mb-5'>
+            @for($i = 0; $i < 8; $i++)
+              <div class='col mb-5'>
               <div class="card">
-                <h5 class='card-header'> EDNL 1</h5>
+                <h5 class='card-header'> EDNL {{$i + 1}}</h5>
                 <img class="card-img-top" src="https://www.grapheverywhere.com/wp-content/uploads/2019/07/pic004.jpg" />
               </div>
             </div>
-
-            <div class='col mb-5'>
-              <div class="card">
-                <h5 class='card-header'> EDNL 2</h5>
-                <img class="card-img-top" src="https://www.grapheverywhere.com/wp-content/uploads/2019/07/pic004.jpg" />
-              </div>
-            </div>
-
-            <div class='col mb-5'>
-              <div class="card">
-                <h5 class='card-header'> EDNL 3</h5>
-                <img class="card-img-top" src="https://www.grapheverywhere.com/wp-content/uploads/2019/07/pic004.jpg" />
-              </div>
-            </div>
-            
-            <div class='col mb-5'>
-              <div class="card">
-                <h5 class='card-header'> EDNL 4</h5>
-                <img class="card-img-top" src="https://www.grapheverywhere.com/wp-content/uploads/2019/07/pic004.jpg" />
-              </div>
-            </div>
-
-            <div class='col mb-5'>
-              <div class="card">
-                <h5 class='card-header'> EDNL 5</h5>
-                <img class="card-img-top" src="https://www.grapheverywhere.com/wp-content/uploads/2019/07/pic004.jpg" />
-              </div>
-            </div>
-
-            <div class='col mb-5'>
-              <div class="card">
-                <h5 class='card-header'> EDNL 6</h5>
-                <img class="card-img-top" src="https://www.grapheverywhere.com/wp-content/uploads/2019/07/pic004.jpg" />
-              </div>
-            </div>
-
-            <div class='col mb-5'>
-              <div class="card">
-                <h5 class='card-header'> EDNL 7</h5>
-                <img class="card-img-top" src="https://www.grapheverywhere.com/wp-content/uploads/2019/07/pic004.jpg" />
-              </div>
-            </div>
-
-            <div class='col mb-5'>
-              <div class="card">
-                <h5 class='card-header'> EDNL 8</h5>
-                <img class="card-img-top" src="https://www.grapheverywhere.com/wp-content/uploads/2019/07/pic004.jpg" />
-              </div>
-            </div>
-
-            
+            @endfor
 
           </div>
 
