62b799a04cae5f11b7c4a9598233730054c2906d
Alvaro Santos <62333521+EviLAsRz@users.noreply.github.com>
2022-10-25 17:24:23 +0200

    :sparkles:Migrate Product completado

---
+++ ProyectoINE2023/app/Models/Product.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    use HasFactory;
+
+    static function newProducts() {
+
+        $sNow = date('Y-m-d H:i:s');
+        $sNextWeek = date('Y-m-d H:i:s', strtotime($sNow . '+ 1 week')); 
+
+        return Product::where(DB::raw(
+            'date_format(updated_at, "%Y-%m-%d")'),
+            '>=', date('Y-m-d', strtotime($sNow)))->where('updated_at', '<=', date('Y-m-d',
+            strtotime($sNextWeek)))->get();
+    }
+}
