f20539dcb180b948ee1571e6da79fc94a223b649
Antonio Roldán Andrade <48240649+Mry3ll0w@users.noreply.github.com>
2022-10-29 12:04:41 +0200

    subida

--- ProyectoINE2023/app/Models/Product.php
+++ ProyectoINE2023/app/Models/Product.php
@@ -9,14 +9,17 @@ class Product extends Model
 {
     use HasFactory;
 
-    static function newProducts() {
+    static function NewProducts() {
 
         $sNow = date('Y-m-d H:i:s');
         $sNextWeek = date('Y-m-d H:i:s', strtotime($sNow . '+ 1 week')); 
 
         return Product::where(DB::raw(
             'date_format(updated_at, "%Y-%m-%d")'),
-            '>=', date('Y-m-d', strtotime($sNow)))->where('updated_at', '<=', date('Y-m-d',
-            strtotime($sNextWeek)))->get();
+            '>=', date('Y-m-d', strtotime($sNow)))->where('updated_at', '<=', 
+            date('Y-m-d',strtotime($sNextWeek)))->get();
     }
+    
+    
+
 }
