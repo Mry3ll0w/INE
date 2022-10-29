d2478a219641562da41a38a770b3b4ba647e3105
Antonio Roldán Andrade <48240649+Mry3ll0w@users.noreply.github.com>
2022-10-29 13:45:06 +0200

    css listo

--- ProyectoINE2023/resources/views/welcome.blade.php
+++ ProyectoINE2023/resources/views/welcome.blade.php
@@ -5,14 +5,25 @@
         <div class='container px-4 px-lg-5 mt-5'>
           <div class='row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 '>
             
-            @for($i = 0; $i < 8; $i++)
+            @foreach ($aProduct_offering as $i )
               <div class='col mb-5'>
-              <div class="card">
-                <h5 class='card-header'> EDNL {{$i + 1}}</h5>
-                <img class="card-img-top" src="https://www.grapheverywhere.com/wp-content/uploads/2019/07/pic004.jpg" />
-              </div>
+              <div class="card d-flex">
+                <h5 class="card-header" style="color: red"> Descuento: {{$i->discountPercent}}% </h5>
+                <div class="card-body">
+                  <h5 class='card-title d-flex justify-content-center'> {{$i->name}}</h5>
+                  <img class="card-img-top mb-3" src="{{$i->imgUrl}}" />
+                  <p class='card-text'>{{$i->description}} </p>
+                </div>
+                <h5 class="card-footer m-2 border d-flex justify-content-center">
+                  <span style="text-decoration:line-through">{{round($i->pricePerCredit * 6,2)}}€ </span>
+                  
+                  <img style="margin-left:10px; margin-right:10px;" src="https://img.icons8.com/flat-round/23/000000/thick-long-right-arrow--v1.png"/>
+                  
+                  <span style="color: red"> {{round(($i->pricePerCredit * ((100-$i->discountPercent)/100))*6,2)}}€</span>
+                </h5>
+                </div>
             </div>
-            @endfor
+            @endforeach
           </div>    
       </section>
 @endsection
