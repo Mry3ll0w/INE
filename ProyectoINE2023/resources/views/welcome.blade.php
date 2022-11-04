d7388c396bfdab7084cf35079c4b1f0b4f6c42f5
Antonio Roldán Andrade <48240649+Mry3ll0w@users.noreply.github.com>
2022-11-04 15:39:52 +0100

    temp

--- ProyectoINE2023/resources/views/welcome.blade.php
+++ ProyectoINE2023/resources/views/welcome.blade.php
@@ -1,7 +1,7 @@
 @extends('templates.master')
 
 @section('contenido')
-     <section class='py-5'>
+    <section class='py-5'>
         <div class='container px-4 px-lg-5 mt-5'>
           <div class='row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 '>
             
--- ProyectoINE2023/routes/web.php
+++ ProyectoINE2023/routes/web.php
@@ -13,15 +13,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [ProductController::class, 'welcome']) ->name('home');
-
+Route::get("/", [ProductController::class, "welcome"])->name("home");
 
 Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified'
+    "auth:sanctum",
+    config("jetstream.auth_session"),
+    "verified",
 ])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
+    Route::get("/dashboard", function () {
+        return view("dashboard");
+    })->name("dashboard");
 });
